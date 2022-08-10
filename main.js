6758ef71261563d8ee6448b7d84766f09432e537
Darko Bazalac <bazalac.darko@outlook.com>
2022-08-10 22:10:18 +0200

    Added About and Projects Sections

--- main.js
+++ main.js
@@ -257,3 +257,156 @@ function darkModeToggle() {
   );
   sunMoonContainer.style.setProperty("--rotation", currentRotation + 180);
 }
+
+// PROJECTS
+const projects = [
+  {
+    id: 1,
+    title: "pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./imgs/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "lunch",
+    category: "lunch",
+    price: 13.99,
+    img: "./imgs/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./imgs/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "breakfast",
+    category: "breakfast",
+    price: 20.99,
+    img: "./imgs/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg burger",
+    category: "lunch",
+    price: 22.99,
+    img: "./imgs/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./imgs/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon burger",
+    category: "breakfast",
+    price: 8.99,
+    img: "./imgs/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./imgs/item-8.jpeg",
+    desc: `loreon it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut `,
+  },
+  {
+    id: 9,
+    title: "quarantine shake",
+    category: "shakes",
+    price: 16.99,
+    img: "./imgs/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "stake dinner",
+    category: "dinner",
+    price: 36.99,
+    img: "./imgs/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+//
+
+const sectionCenter = document.querySelector(".project-section-center");
+const container = document.querySelector(".project-btn-container");
+
+//load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayProjectItems(projects);
+  displayProjectButtons();
+});
+
+function displayProjectItems(projectItems) {
+  let displayProject = projectItems.map(function (item) {
+    return `<article class="project-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <div class="social-icons">
+                <a><i class="fa-solid fa-desktop" style="font-size: 1.4rem;"></i></a>
+                <a><i class="fa-brands fa-github"></i></a>
+              </div>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayProject = displayProject.join("");
+
+  sectionCenter.innerHTML = displayProject;
+}
+
+function displayProjectButtons() {
+  const categories = projects.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"],
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>${category} </button>`;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtn = document.querySelectorAll(".filter-btn");
+
+  //filter items
+  filterBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const projectCategory = projects.filter(function (projectItem) {
+        if (projectItem.category === category) {
+          return projectItem;
+        }
+      });
+      if (category === "all") {
+        displayProjectItems(projects);
+      } else {
+        displayProjectItems(projectCategory);
+      }
+    });
+  });
+}
